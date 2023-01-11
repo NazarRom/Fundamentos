767e8afc08a8ee64035b5e6da7fabd6b946ea99c
NazarRom <nazarrom321@gmail.com>
2023-01-11 09:56:55 +0100

    Add project files.

---
+++ Fundamentos/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Fundamentos
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btnPulsar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(112, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nombre:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(199, 63);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(196, 23);
+            this.txtNombre.TabIndex = 2;
+            // 
+            // btnPulsar
+            // 
+            this.btnPulsar.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnPulsar.Location = new System.Drawing.Point(187, 129);
+            this.btnPulsar.Name = "btnPulsar";
+            this.btnPulsar.Size = new System.Drawing.Size(178, 47);
+            this.btnPulsar.TabIndex = 3;
+            this.btnPulsar.Text = "Pulsar";
+            this.btnPulsar.UseVisualStyleBackColor = false;
+            this.btnPulsar.Click += new System.EventHandler(this.btnPulsar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(953, 617);
+            this.Controls.Add(this.btnPulsar);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Label label1;
+        private TextBox txtNombre;
+        private Button btnPulsar;
+    }
+}
---
+++ Fundamentos/Form1.cs
@@ -0,0 +1,31 @@
+namespace Fundamentos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPulsar_Click(object sender, EventArgs e)
+        {   
+            //aqui escribiremos nuestras acciones
+            this.txtNombre.Width = 50;
+            this.btnPulsar.Text = "Botón pulsado!!!";
+            //tenemos propiedades que son de tipo objeto
+            this.txtNombre.Location = new Point(70, 100);
+            //tenemos propiedades de tipo enumeradas
+            //una enumeracion son una serie de valores 
+            //que se representan de forma grafica y ayudan al
+            //programador
+            this.txtNombre.TextAlign = HorizontalAlignment.Center;
+            this.BackColor = Color.Tomato;
+
+        }
+    }
+}
